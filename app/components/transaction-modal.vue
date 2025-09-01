340789a55e8aecf668fd9b427da959faa812fccc
Denys Vladymyrov <link_amd@yahoo.com>
2025-09-01 22:29:14 +0300

    added zod schema

--- app/components/transaction-modal.vue
+++ app/components/transaction-modal.vue
@@ -6,9 +6,9 @@
                     Add Transaction
                 </template>
 
-                <UForm :state="state">
+                <UForm :state="state" :schema="schema" ref="form" @submit="save">
                     <UFormField :required="true" label="Transaction Type" name="type" class="mb-4">
-                        <USelect placeholder="Select the transaction type" :items="[...TRANSACTIONS]" class="w-full" />
+                        <USelect placeholder="Select the transaction type" :items="[...TRANSACTIONS]" class="w-full" v-model="state.type" />
                     </UFormField>
 
                     <UFormField :required="true" label="Amount" name="amount" class="mb-4">
@@ -23,7 +23,7 @@
                         <UInput placeholder="Description" class="w-full" v-model="state.description" />
                     </UFormField>
 
-                    <UFormField :required="true" hint="Optional" label="Category" name="category" class="mb-4">
+                    <UFormField :required="true" hint="Optional" label="Category" name="category" class="mb-4" v-if="state.type === TransactionTypeEnum.Expense">
                         <USelect placeholder="Category" :items="[...CATEGORIES]" class="w-full" v-model="state.category" />
                     </UFormField>
 
@@ -42,8 +42,10 @@
 </template>
 
 <script setup lang="ts">
-import { type ITransaction } from '~/types';
+import { z } from 'zod'
+import {type ITransaction, type TransactionType, TransactionTypeEnum} from '~/types';
 import { TRANSACTIONS, CATEGORIES } from '~/constants';
+import type {FormSchema} from "#ui/types/form";
 
 const props = defineProps<{
     modelValue: boolean
@@ -58,13 +60,48 @@ const isOpen = computed<boolean>({
     set: (value) => emit('update:modelValue', value),
 });
 
-type LocalTransaction = Omit<ITransaction, 'id'>
+type LocalTransaction = Omit<ITransaction, 'id' | 'type'> & {
+    type?: TransactionType
+}
 
 const state = ref<LocalTransaction>({
     created_at: '',
     amount: 0,
-    type: 'Income',
+    type: undefined,
     description: '',
     category: undefined
+});
+
+const defaultSchema = z.object({
+    created_at: z.string()
+        .min(1, 'Please select a date')
+        .refine(val => !isNaN(Date.parse(val)), {
+            message: 'Invalid date'
+        }),
+    description: z.string().optional(),
+    amount: z.number().positive('Amount needs to be more than 0')
+});
+const incomeSchema = z.object({
+    type: z.literal('Income')
+});
+const expenseSchema = z.object({
+    type: z.literal('Expense'),
+    category: z.enum(CATEGORIES)
+});
+const investmentSchema = z.object({
+    type: z.literal('Investment')
 })
+const savingSchema = z.object({
+    type: z.literal('Saving')
+});
+const schema = z.intersection(
+    z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
+    defaultSchema
+);
+
+const form = ref<FormSchema>();
+
+const save = async () => {
+    form.value.validate();
+}
 </script>
